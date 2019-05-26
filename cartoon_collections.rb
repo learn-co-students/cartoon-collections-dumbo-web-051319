7df7806788b8b4a192c7675d270164d9bb08b9dc
L.R.L <DaleLoreNY@gmail.com>
2019-05-26 15:39:48 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,24 +1,22 @@
 
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf, index|
-    puts "#{index + 1}. #{dwarf}"
-  end
-end
+    dwarves.each_with_index do |dwarf,i|
+        puts "#{i+1}. #{dwarf}"
+    end
+end 
 
 def summon_captain_planet(planeteer_calls)
- planeteer_calls.map { |call| call.capitalize + '!' }
+    planeteer_calls.map { |call| call.capitalize + '!' }
 end
-
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? { |call| call.length > 4 }
-  #ask in class why I wouldnt add the do 
+    planeteer_calls.any? { |call| call.length > 4}
 end
 
+def find_the_cheese(cheese_list)
+    cheeses_to_find = ["cheddar", "gouda", "camembert"]
 
-def find_the_cheese(cheeses)
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheeses.include? (cheddar)
-  end
-end
+    cheese_list.find do |cheese|
+        cheeses_to_find.include?(cheese)
+    end
+end 
